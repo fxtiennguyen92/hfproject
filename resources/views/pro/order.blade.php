4278e1ba7b2a9ab61d69ea597f1f6c6f6ef1475e
tandatx <tandatx@gmail.com>
2018-02-02 17:10:56 +0700

    Fix layout

--- resources/views/pro/order.blade.php
+++ resources/views/pro/order.blade.php
@@ -1,94 +1,85 @@
-@extends('template.index')
+@extends('template.index') @push('stylesheets')
+<script>
+  $(document).ready(function() {
+    $('.quoted-price').number({
+      'containerClass': 'number-style',
+      'minus': 'number-minus',
+      'plus': 'number-plus',
+      'containerTag': 'div',
+      'btnTag': 'span'
+    });
 
-@push('stylesheets')
-	<script>
-		$(document).ready(function() {
-			$('.quoted-price').number({
-				'containerClass' : 'number-style',
-				'minus' : 'number-minus',
-				'plus' : 'number-plus',
-				'containerTag' : 'div',
-				'btnTag' : 'span'
-			});
-			
-			@if ($quotedPrice)
-				$('input[name=price]').val('{{ $quotedPrice->price }}');
-			@else
-				$('input[name=price]').val('0');
-			@endif
-			$('.quoted-price').val(accounting.formatMoney($('input[name=price]').val()));
-		});
+    @if($quotedPrice)
+    $('input[name=price]').val('{{ $quotedPrice->price }}');
+    @else
+    $('input[name=price]').val('0');
+    @endif
+    $('.quoted-price').val(accounting.formatMoney($('input[name=price]').val()));
+  });
 
-		$('#frmMain').validate({
-			submit: {
-				callback: {
-					onSubmit: function() {
-						$.ajax({
-							type: 'POST',
-							url: '{{ route("quote_price") }}',
-							data: $('#frmMain').serialize(),
-							success: function(response) {
-								
-							},
-							error: function(xhr) {
-								if (xhr.status == 400) {
-									$.notify({
-										title: '<strong>Thất bại! </strong>',
-										message: 'Đơn hàng này đã đóng hoặc đã bị hủy.'
-									},{
-										type: 'danger',
-									});
-								} else if (xhr.status == 417) {
-									$.notify({
-										title: '<strong>Thất bại! </strong>',
-										message: 'Đơn hàng này đã .'
-									},{
-										type: 'danger'
-									});
-								};
-							}
-						});
-					}
-				}
-			}
-		});
-	</script>
-@endpush
+  $('#frmMain').validate({
+    submit: {
+      callback: {
+        onSubmit: function() {
+          $.ajax({
+            type: 'POST',
+            url: '{{ route("quote_price") }}',
+            data: $('#frmMain').serialize(),
+            success: function(response) {
 
-@section('title')
-
-@endsection
-
-@section('content')
+            },
+            error: function(xhr) {
+              if (xhr.status == 400) {
+                $.notify({
+                  title: '<strong>Thất bại! </strong>',
+                  message: 'Đơn hàng này đã đóng hoặc đã bị hủy.'
+                }, {
+                  type: 'danger',
+                });
+              } else if (xhr.status == 417) {
+                $.notify({
+                  title: '<strong>Thất bại! </strong>',
+                  message: 'Đơn hàng này đã .'
+                }, {
+                  type: 'danger'
+                });
+              };
+            }
+          });
+        }
+      }
+    }
+  });
+</script>
+@endpush @section('title') @endsection @section('content')
 <section class="page-content">
-	<form id="frmMain" method="POST" action="{{ route('quote_price') }}">
-		<input name="inpPrice" class="quoted-price" value="10000" step="10000" min="10000" max="">
-		<input name="price" class="basic-quoted-price" value="0" type="hidden"/>
-		
-		<div class="row">
-			<div class="order-info col-md-10 col-sm-10 col-sx-10">
-				<img src="../storage/app/u/{{ $order->user_id }}/{{ $order->user_avatar }}">
-				<label>{{ $order->user_name }}</label>
-				<div class="order-req">
-					{{ $order->short_requirements }}
-				</div>
-				<p><span class="icmn-location"></span> {{ $order->address }}</p>
-			</div>
-		</div>
-		
-		@if ($quotedPrice)
-		<button id="btnSubmit" type="submit" class="btn btn-danger width-150">
+  <form id="frmMain" method="POST" action="{{ route('quote_price') }}">
+    <input name="inpPrice" class="quoted-price" value="10000" step="10000" min="10000" max="">
+    <input name="price" class="basic-quoted-price" value="0" type="hidden" />
+
+    <div class="row">
+      <div class="order-info col-md-10 col-sm-10 col-sx-10">
+        <img src="../storage/app/u/{{ $order->user_id }}/{{ $order->user_avatar }}">
+        <label>{{ $order->user_name }}</label>
+        <div class="order-req">
+          {{ $order->short_requirements }}
+        </div>
+        <p><span class="icmn-location"></span> {{ $order->address }}</p>
+      </div>
+    </div>
+
+    @if ($quotedPrice)
+    <button id="btnSubmit" type="submit" class="btn btn-danger width-150">
 			Xóa báo giá
-		</button>
-		@endif
-		<button id="btnSubmit" type="submit" class="btn btn-primary width-150">
+		</button> @endif
+    <button id="btnSubmit" type="submit" class="btn btn-primary width-150">
 			@if ($quotedPrice)
 			Báo giá lại
 			@else
 			Báo giá
 			@endif
 		</button>
-		<input type="hidden" name="_token" value="{{ csrf_token() }}" />
-	</form>
+    <input type="hidden" name="_token" value="{{ csrf_token() }}" />
+  </form>
 </section>
 @endsection
--- resources/views/pro/orders.blade.php
+++ resources/views/pro/orders.blade.php
@@ -1,93 +1,92 @@
-@extends('template.index')
+@extends('template.index') @push('stylesheets')
+<!--<link rel="stylesheet" type="text/css" href="css/orders.css">-->
+<style>
+  body {
+    background: #f4f4f4;
+  }
 
-@push('stylesheets')
-	<link rel="stylesheet" type="text/css" href="css/orders.css">
-@endpush
-
-@section('title')
-
-@endsection
-
-@section('content')
+</style>
+@endpush @section('title') @endsection @section('content')
 <section class="page-content">
-	<div class="orders col-md-12 col-sm-12 col-sx-12">
-		<div class="margin-bottom-50">
-			<div class="nav-tabs-horizontal">
-				<ul class="nav nav-tabs" role="tablist">
-					<li class="nav-item">
-						<a class="nav-link active" href="javascript: void(0);" data-toggle="tab" data-target="#tabNew" role="tab">Đơn hàng mới</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="javascript: void(0);" data-toggle="tab" data-target="#tabQuoted" role="tab">Đã báo giá </a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="javascript: void(0);" data-toggle="tab" data-target="#tabComplete" role="tab">Lịch sử</a>
-					</li>
-				</ul>
-				<div class="tab-content padding-vertical-20">
-					<div class="tab-pane active" id="tabNew" role="tabpanel">
-						@foreach ($newOrders as $order)
-						<div class="row order-row" onclick="location.href='{{ route('view_pro_order', ['orderId' => $order->id]) }}'">
-							<div class="order-info col-md-10 col-sm-10 col-sx-10">
-								<label>{{ $order->user_name }}</label>
-								<div class="order-req">
-									{{ $order->short_requirements }}
-								</div>
-								<p><span class="icmn-location"></span> {{ $order->address }}</p>
-							</div>
-							<div class="order-state col-md-2 col-sm-2 col-sx-2">
-								@if ($order->est_excute_at_string)
-								<span class="state-est-time">{{ $order->est_excute_at_string }}</span>
-								@else
-								<span class="state-now">Ngay lập tức</span>
-								@endif
-							</div>
-						</div>
-						@endforeach
-					</div>
-					<div class="tab-pane" id="tabQuoted" role="tabpanel">
-						@foreach ($quotedOrders as $order)
-						<div class="row order-row" onclick="location.href='{{ route('view_pro_order', ['orderId' => $order->id]) }}'">
-							<div class="order-info col-md-10 col-sm-10 col-sx-10">
-								<label>{{ $order->user_name }}</label>
-								<div class="order-req">
-									{{ $order->short_requirements }}
-								</div>
-								<p><span class="icmn-location"></span> {{ $order->address }}</p>
-							</div>
-							<div class="order-state col-md-2 col-sm-2 col-sx-2">
-								@if ($order->time_state == '0')
-								<span class="state-now">Ngay lập tức</span>
-								@else
-								<span class="state-confirm">Hẹn thời gian</span>
-								@endif
-							</div>
-						</div>
-						@endforeach
-					</div>
-					<div class="tab-pane" id="tabComplete" role="tabpanel">
-						@foreach ($newOrders as $order)
-						<div class="row order-row" onclick="location.href='{{ route('view_pro_order', ['orderId' => $order->id]) }}'">
-							<div class="order-info col-md-10 col-sm-10 col-sx-10">
-								<label>{{ $order->user_name }}</label>
-								<div class="order-req">
-									{{ $order->short_requirements }}
-								</div>
-								<p><span class="icmn-location"></span> {{ $order->address }}</p>
-							</div>
-							<div class="order-state col-md-2 col-sm-2 col-sx-2">
-								@if ($order->time_state == '0')
-								<span class="state-now">Ngay lập tức</span>
-								@else
-								<span class="state-confirm">Hẹn thời gian</span>
-								@endif
-							</div>
-						</div>
-						@endforeach
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+  <div class="hf-wrapper">
+    <div class="orders col-md-12 col-sm-12 col-sx-12">
+      <div class="margin-bottom-50">
+        <div class="nav-tabs-horizontal">
+          <ul class="nav nav-tabs" role="tablist">
+            <li class="nav-item">
+              <a class="nav-link active" href="javascript: void(0);" data-toggle="tab" data-target="#tabNew" role="tab">Đơn hàng mới</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="javascript: void(0);" data-toggle="tab" data-target="#tabQuoted" role="tab">Đã báo giá </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="javascript: void(0);" data-toggle="tab" data-target="#tabComplete" role="tab">Lịch sử</a>
+            </li>
+          </ul>
+          <div class="tab-content padding-vertical-20">
+            <div class="tab-pane active" id="tabNew" role="tabpanel">
+              @foreach ($newOrders as $order)
+              <div class="col-md-4 col-sm-6">
+                <div class="item-wrapper">
+                  <div class="row order-row" onclick="location.href='{{ route('view_pro_order', ['orderId' => $order->id]) }}'">
+                    <div class="order-info">
+                      <label><img class="avt" src="http://innovatik.payo-themes.com/wp-content/uploads/2017/11/lawn-team03.jpg"/> {{ $order->user_name }}</label>
+                      <div class="order-req">
+                        {{ $order->short_requirements }}
+                      </div>
+                      <div class="order-address"><i class="material-icons">&#xE0C8;</i> {{ $order->address }}</div>
+                    </div>
+                    <div class="order-state">
+                      @if ($order->est_excute_at_string)
+                      <span class="state-est-time"><i class="material-icons">&#xE855;</i> {{ $order->est_excute_at_string }}</span> @else
+                      <span class="state-now"><i class="material-icons">&#xE3E7;</i>Ngay lập tức</span> @endif
+                    </div>
+                  </div>
+                </div>
+              </div>
+              @endforeach
+            </div>
+            <div class="tab-pane" id="tabQuoted" role="tabpanel">
+              @foreach ($quotedOrders as $order)
+              <div class="row order-row" onclick="location.href='{{ route('view_pro_order', ['orderId' => $order->id]) }}'">
+                <div class="order-info col-md-10 col-sm-10 col-sx-10">
+                  <label>{{ $order->user_name }}</label>
+                  <div class="order-req">
+                    {{ $order->short_requirements }}
+                  </div>
+                  <p><span class="icmn-location"></span> {{ $order->address }}</p>
+                </div>
+                <div class="order-state col-md-2 col-sm-2 col-sx-2">
+                  @if ($order->time_state == '0')
+                  <span class="state-now">Ngay lập tức</span> @else
+                  <span class="state-confirm">Hẹn thời gian</span> @endif
+                </div>
+              </div>
+              @endforeach
+            </div>
+            <div class="tab-pane" id="tabComplete" role="tabpanel">
+              @foreach ($newOrders as $order)
+              <div class="row order-row" onclick="location.href='{{ route('view_pro_order', ['orderId' => $order->id]) }}'">
+                <div class="order-info col-md-10 col-sm-10 col-sx-10">
+                  <label>{{ $order->user_name }}</label>
+                  <div class="order-req">
+                    {{ $order->short_requirements }}
+                  </div>
+                  <p><span class="icmn-location"></span> {{ $order->address }}</p>
+                </div>
+                <div class="order-state col-md-2 col-sm-2 col-sx-2">
+                  @if ($order->time_state == '0')
+                  <span class="state-now">Ngay lập tức</span> @else
+                  <span class="state-confirm">Hẹn thời gian</span> @endif
+                </div>
+              </div>
+              @endforeach
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
 </section>
 @endsection
