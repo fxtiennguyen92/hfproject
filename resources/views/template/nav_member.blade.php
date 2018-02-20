52dce6b59bb47067dc695dd4cef4834e5c90253f
tandatx <tandatx@gmail.com>
2018-02-21 02:22:31 +0700

    fix css nav_member.blade

--- resources/views/template/nav_member.blade.php
+++ resources/views/template/nav_member.blade.php
@@ -1,39 +1,40 @@
 <!-- Navbar Member -->
 <nav class="top-menu">
-	<div class="menu-icon-container hidden-md-up">
-	<div class="animate-menu-button left-menu-toggle">
-		<div>
-		<!-- -->
-		</div>
-	</div>
-	</div>
-	<div class="menu">
-		<div class="menu-info-block">
-			<div class="left">
-			<div class="">
-				<a href="https://handfree.co"><img src="img/hf-logo.png" class="navbar-logo"></a>
-			</div>
-			</div>
-		</div>
-		<div class="menu-user-block">
-			<div class="dropdown dropdown-avatar">
-				<a href="javascript: void(0);" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+  <div class="menu-icon-container hidden-md-up">
+    <div class="animate-menu-button left-menu-toggle">
+      <div>
+        <!-- -->
+      </div>
+    </div>
+  </div>
+  <div class="menu">
+    <div class="menu-info-block">
+      <div class="left">
+        <div class="hf-logo">
+          <a href="https://handfree.co"><img src="img/hf-logo.png" class="navbar-logo"></a>
+        </div>
+      </div>
+    </div>
+    <div class="menu-user-block">
+      <div class="dropdown dropdown-avatar">
+        <a href="javascript: void(0);" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+          <span class="user-name">Chào, Đạt!</span>
 					<span class="avatar">
 						<img src="../storage/app/u/{{ auth()->user()->id }}/{{ auth()->user()->avatar }}" alt="User">
 					</span>
+					
 				</a>
-				<ul class="dropdown-menu dropdown-menu-right" aria-labelledby="" role="menu">
-					@if (auth()->user()->role == 2)
-					<a class="dropdown-item" href="{{ route('signup_pro') }}"><i class="dropdown-icon material-icons">face</i> Đăng ký Đối Tác</a>
-					<a class="dropdown-item" href="{{ route('pro_list_page') }}"><i class="dropdown-icon material-icons">list</i> Danh sách Đối Tác</a>
-					<div class="dropdown-divider"></div>
-					<a class="dropdown-item" href="{{ route('company_page') }}"><i class="dropdown-icon material-icons">work</i> Đăng ký Doanh Nghiệp</a>
-					<a class="dropdown-item" href="{{ route('company_list_page') }}"><i class="dropdown-icon material-icons">list</i> Danh sách Doanh Nghiệp</a>
-					<div class="dropdown-divider"></div>
-					<a class="dropdown-item" href="{{ route('logout') }}"><i class="dropdown-icon material-icons">power_settings_new</i> Đăng Xuất</a>
-					@endif
-				</ul>
-			</div>
-		</div>
-	</div>
+        <ul class="dropdown-menu dropdown-menu-right" aria-labelledby="" role="menu">
+          @if (auth()->user()->role == 2)
+          <a class="dropdown-item" href="{{ route('signup_pro') }}"><i class="dropdown-icon material-icons">face</i> Đăng ký Đối Tác</a>
+          <a class="dropdown-item" href="{{ route('pro_list_page') }}"><i class="dropdown-icon material-icons">list</i> Danh sách Đối Tác</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="{{ route('company_page') }}"><i class="dropdown-icon material-icons">work</i> Đăng ký Doanh Nghiệp</a>
+          <a class="dropdown-item" href="{{ route('company_list_page') }}"><i class="dropdown-icon material-icons">list</i> Danh sách Doanh Nghiệp</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="{{ route('logout') }}"><i class="dropdown-icon material-icons">power_settings_new</i> Đăng Xuất</a> @endif
+        </ul>
+      </div>
+    </div>
+  </div>
 </nav>
