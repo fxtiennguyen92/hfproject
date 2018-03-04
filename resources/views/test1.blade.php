fd68d4c0c53c67d83b83aae9e0efed82c14bf62e
tandatx <tandatx@gmail.com>
2018-03-04 18:07:24 +0700

    update test1.blade

--- resources/views/test1.blade.php
+++ resources/views/test1.blade.php
@@ -1,8 +1,15 @@
 @extends('template.index') @push('stylesheets')
 <script>
+
+
 </script>
 @endpush @section('title') Trang điều khiển @endsection @section('content')
-<section class="content-body">
-
+<section class="content-body user-menu">
+  <ul class="list-menu">
+    <li><a href="#"><i class="material-icons">&#xE887;</i> Trợ giúp</a></li>
+    <li><a href="#"><i class="material-icons">&#xE227;</i> Trở thành đối tác</a></li>
+    <li><a href="#"><i class="material-icons">&#xE8B8;</i> Cài đặt</a></li>
+    <li><a href="#"><i class="material-icons">&#xE879;</i> Đăng xuất</a></li>
+  </ul>
 </section>
 @endsection
