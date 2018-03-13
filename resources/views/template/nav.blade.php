11fa10a8910c7fec1b472d85e036d0e88aeccec2
tandatx <tandatx@gmail.com>
2018-03-14 00:13:31 +0700

    update display email nav

--- resources/views/template/nav.blade.php
+++ resources/views/template/nav.blade.php
@@ -9,19 +9,19 @@
         </div>
       </div>
       <div class="icon-menu-wrapper">
-        <ul class="icon-menu" >
+        <ul class="icon-menu">
           <li class="icon-menu-user @if(isset($nav)) @if($nav == 'control') active @endif @endif">
             <a href="{{ route('control') }}">
-              <span>@if (auth()->check()) {{ auth()->user()->email }} @endif<i class="material-icons">&#xE7FD;</i></span>
+              <span>@if (auth()->check()) {{ auth()->user()->email }} @endif</span><i class="material-icons">&#xE7FD;</i>
             </a>
           </li>
           <li class="icon-menu-notification @if(isset($nav)) @if($nav == 'noti') active @endif @endif">
-            <a href="javascript:void(0)" >
+            <a href="javascript:void(0)">
               <span><i class="material-icons">&#xE80B;</i></span>
             </a>
           </li>
           <li class="icon-menu-order @if(isset($nav)) @if($nav == 'order') active @endif @endif">
-            <a href="{{ route('order_list_page') }}" >
+            <a href="{{ route('order_list_page') }}">
               <span><i class="material-icons">&#xE8B0;</i></span>
             </a>
           </li>
