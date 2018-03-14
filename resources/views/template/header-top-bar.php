2009897046bacb24f94b40cc0e8be5936935211b
tandatx <tandatx@gmail.com>
2018-03-14 11:32:46 +0700

    add header-top-bar

---
+++ resources/views/template/header-top-bar.php
@@ -0,0 +1,4 @@
+<nav class="navbar header-top-bar navbar-fixed-top">
+  <a class="arrow-back" href="#"><i class="material-icons">&#xE5C4;</i></a>
+  <h3 class="navbar-title">Lorem ipsum dolor</h3>
+</nav>
